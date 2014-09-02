c2e6fe50022baf945a0a49a97cb9487633a7758e
Stefan Korunovski <korun_stefan@hotmail.com>
2014-09-02 12:55:54 +0000

    ignore na c9 i izlemi vo game.js

--- app/game.js
+++ app/game.js
@@ -1,8 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
 });
+var sprite;
+var cursors;
 
 function preload() {}
 
@@ -16,6 +19,7 @@ function create() {
     // game.physics.arcade.enable(wowow);
 
     game.stage.backgroundColor = '#71c5cf';
+    game.world.setBounds(0, 0, game.width * 3, game.height);
     var graphics = game.add.graphics(0, 0);
 
     graphics.beginFill('#000000', 1);
@@ -24,14 +28,35 @@ function create() {
     graphics.alpha = 1;
     graphics.endFill();
 
-    var sprite = game.add.sprite(100, 100);
+    sprite = game.add.sprite(100, 100);
     sprite.addChild(graphics);
-    
+
     game.physics.arcade.enable(sprite);
-    sprite.body.velocity.x = 50;
+
+    game.camera.follow(sprite);
+    cursors = game.input.keyboard.createCursorKeys();
 
 }
 
 function update() {
+    if (cursors.left.isDown) {
+        sprite.x -= 4;
+    }
+    else if (cursors.right.isDown) {
+        sprite.x += 4;
+    }
 
+    if (cursors.up.isDown) {
+        sprite.y -= 4;
+    }
+    else if (cursors.down.isDown) {
+        sprite.y += 4;
+    }
+
+}
+
+function render() {
+    game.debug.cameraInfo(game.camera, 500, 32);
+    game.debug.spriteCoords(sprite, 32, 32);
+    // game.debug.physicsBody(sprite.body);
 }
