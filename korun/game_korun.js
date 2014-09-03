3a570f9f44a97f096e48d5120f3a4e9690245e54
Stefan <korun_stefan@hotmail.com>
2014-09-03 19:33:04 +0200

    da ojt levo desno

--- korun/game_korun.js
+++ korun/game_korun.js
@@ -8,45 +8,45 @@ var sprite;
 var cursors;
 
 function preload() {
+//    this.load.image('background', 'assets/flappy/background.png');
 }
 
 function create() {
     game.stage.backgroundColor = '#71c5cf';
 
-    game.world.setBounds(0, 0, game.width * 3, game.height);
+
+    game.world.setBounds(0, 0, game.width, game.height);
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.setBoundsToWorld(true, true, true, true, false); // fix za da odbivat od granici
 
     game.physics.p2.gravity.y = 200;
     game.physics.p2.restitution = 0.8;
 
-    var bmd = game.add.bitmapData(100, 100);
+    var bmd = game.add.bitmapData(60, 60);
     sprite = game.add.sprite(100, 100, bmd);
     bmd.clear();
     bmd.circle(30, 30, 30);
-    bmd.resize(60, 60);
-    bmd.circle(30, 30, 30);
-    sprite.frame = bmd.textureFrame;
 
     game.physics.p2.enable(sprite);
-    game.camera.follow(sprite);
-    cursors = game.input.keyboard.createCursorKeys();
 
+    game.camera.follow(sprite);
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
     if (cursors.left.isDown) {
-        sprite.x -= 4;
+        sprite.body.velocity.x -= 200;
     }
     else if (cursors.right.isDown) {
-        sprite.x += 4;
+        sprite.body.velocity.x += 200;
     }
 
     if (cursors.up.isDown) {
-        sprite.y -= 4;
+        sprite.body.velocity.y -= 200;
     }
     else if (cursors.down.isDown) {
-        sprite.y += 4;
+        sprite.body.velocity.y += 200;
     }
 
 }
