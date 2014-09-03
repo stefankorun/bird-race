81a6521a5932419c66928f646745619ab67a3ec1
Stefan <korun_stefan@hotmail.com>
2014-09-03 21:52:57 +0200

    Pozadina i letaje so space

--- korun/game_korun.js
+++ korun/game_korun.js
@@ -5,50 +5,46 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     render: render
 });
 var sprite;
-var cursors;
+var space;
 
 function preload() {
-//    this.load.image('background', 'assets/flappy/background.png');
+    this.load.image('background', 'assets/google/cloud-background2.png');
 }
 
 function create() {
     game.stage.backgroundColor = '#71c5cf';
 
-
-    game.world.setBounds(0, 0, game.width, game.height);
+    game.world.setBounds(0, 0, game.width * 3, game.height);
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.setBoundsToWorld(true, true, true, true, false); // fix za da odbivat od granici
 
-    game.physics.p2.gravity.y = 200;
-    game.physics.p2.restitution = 0.8;
+    console.log(game.world);
+    game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
+
+    game.physics.p2.gravity.y = 1000;
+    game.physics.p2.restitution = 0.9;
 
     var bmd = game.add.bitmapData(60, 60);
     sprite = game.add.sprite(100, 100, bmd);
     bmd.clear();
     bmd.circle(30, 30, 30);
+    bmd.fill('#FF00FF')
+
 
     game.physics.p2.enable(sprite);
+    sprite.body.angle = 45;
 
     game.camera.follow(sprite);
 
-    cursors = game.input.keyboard.createCursorKeys();
+    space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
 }
 
 function update() {
-    if (cursors.left.isDown) {
-        sprite.body.velocity.x -= 200;
-    }
-    else if (cursors.right.isDown) {
-        sprite.body.velocity.x += 200;
-    }
-
-    if (cursors.up.isDown) {
+    if (space.isDown) {
         sprite.body.velocity.y -= 200;
     }
-    else if (cursors.down.isDown) {
-        sprite.body.velocity.y += 200;
-    }
-
+    sprite.body.velocity.x = 100;
 }
 
 function render() {
